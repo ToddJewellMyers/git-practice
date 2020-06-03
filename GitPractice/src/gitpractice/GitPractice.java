22bf85a5e6f17f497bc59a401a57c7a9397a7f64
Michelle Cheatham <michelle.cheatham@gmail.com>
2020-06-03 10:13:41 -0400

    Changed to Hello Michelle

--- GitPractice/src/gitpractice/GitPractice.java
+++ GitPractice/src/gitpractice/GitPractice.java
@@ -7,7 +7,7 @@ package gitpractice;
 public class GitPractice {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Michelle!");
     }
     
 }
