5eff971a0fba0f0fb5b7fbe80a3b9d31ce93251b
Michelle Cheatham <michelle.cheatham@gmail.com>
2020-06-03 09:58:23 -0400

    Initial Hello World code

---
+++ GitPractice/src/gitpractice/GitPractice.java
@@ -0,0 +1,13 @@
+package gitpractice;
+
+/**
+ *
+ * @author michelle
+ */
+public class GitPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
